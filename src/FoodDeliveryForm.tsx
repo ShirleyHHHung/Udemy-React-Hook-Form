a47cd95c423369e32e3ba5c0030ca889c2fb8213
ellie <shirley109000@gmail.com>
2026-03-10 14:40:57 +0800

    5-1

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,28 +1,93 @@
 import { useState, type ChangeEvent, type SyntheticEvent } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, type FieldErrors } from "react-hook-form";
+import TextField from "./controls/TextField";
 
 type FoodDeliveryFormType = {
+  orderNo: number;
   customerName: string;
   mobile: string;
+  email: string;
 };
 
 const FoodDeliveryForm = () => {
-  const { register } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FoodDeliveryFormType>({
+    defaultValues: {
+      orderNo: new Date().valueOf(),
+      customerName: "",
+      mobile: "",
+      email: "",
+    },
+    mode: "onChange",
+    criteriaMode: "all",
+  });
+
+  const onSubmit = (formData: FoodDeliveryFormType) => {
+    console.log("formData", formData);
+  };
+
+  const onError = (err: FieldErrors) => {
+    console.log("err", err);
+  };
+
   return (
-    <form autoComplete="off">
-      <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Customer Name"
-          {...register("customerName")}
-        />
-        <label>Customer Name</label>
+    <form
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit, onError)}
+      noValidate
+    >
+      <div className="row mb-2">
+        <div className="col">
+          <div className="form-floating">
+            <TextField label="#order No." disabled {...register("orderNo")} />
+          </div>
+        </div>
+        <div className="col">
+          <TextField
+            label="Mobile"
+            {...register("mobile", {
+              required: "此欄位必填",
+            })}
+            error={errors.mobile}
+          />
+          {/* {errors.mobile?.message &&
+              Object.values(errors.mobile?.types as Record<string, string>).map(
+                (item, index) => (
+                  <div key={index} className="error-feedback">
+                    {item}
+                  </div>
+                ),
+              )} */}
+        </div>
       </div>
-      <div className="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Mobile" />
-        <label>Mobile</label>
+      <div className="row mb-2">
+        <div className="col">
+          <TextField
+            label="Customer Name"
+            {...register("customerName", {
+              required: "此欄位必填",
+            })}
+            error={errors.customerName}
+          />
+        </div>
+        <div className="col">
+          <TextField
+            type="email"
+            label="Email"
+            {...register("email", {
+              pattern: {
+                value: /([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})/,
+                message: "email 格式錯誤",
+              },
+            })}
+            error={errors.email}
+          />
+        </div>
       </div>
+
       <button type="submit" className="btn btn-primary">
         submit
       </button>
---
+++ src/controls/TextField.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, type ForwardedRef } from "react";
+import { type FieldError } from "react-hook-form";
+
+type TextFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  error?: FieldError | undefined;
+};
+
+const TextField = forwardRef(
+  (props: TextFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const { type = "text", className = "", label, error, ...other } = props;
+
+    return (
+      <div className="form-floating">
+        <input
+          type={type}
+          className={`form-control ${className}`}
+          placeholder={label}
+          ref={ref}
+          {...other}
+        />
+        <label>{label}</label>
+        {error && <div className="error-feedback">{error.message}</div>}
+      </div>
+    );
+  },
+);
+
+export default TextField;
